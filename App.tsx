cce11b06ab8744293b1ac26e8998f4b237cfc28e
sthaarekh <arekhshrestha@gmail.com>
2025-04-24 18:13:21 +0545

    Added bottom bar navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,60 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { s } from "react-native-wind";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './src/components/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
+const tabConfig=[
+  {
+    name:"Home",
+    component: HomeScreen,
+    focusedIcon: 'home',
+    unFocusedIcon: 'home-outline',
+    iconComponent: Ionicons
+  },
+  {
+    name:"Profile",
+    component: HomeScreen,
+    focusedIcon: 'settings',
+    unFocusedIcon: 'settings-outline',
+    iconComponent: Ionicons,
+  },
+]
 const App = () => {
+  const Tab = createBottomTabNavigator();
+  const screenOptions = ({ route }) => ({
+    tabBarIcon: ({ focused, color, size }) => {
+      const routeConfig = tabConfig.find(config => config.name === route.name);
+      const iconName = focused
+        ? routeConfig.focusedIcon
+        : routeConfig.unFocusedIcon;
+      const IconComponent = routeConfig.iconComponent;
+  
+      return <IconComponent name={iconName} size={size} color={color} />;
+    },
+          tabBarActiveTintColor:'blue',
+          tabBarInactiveTintColor: 'black',
+          tabBarLabelStyle:{
+            fontSize:14,
+            paddingBottom:5,
+            fontWeight: 600,
+          },
+          tabBarStyle:{
+            height:60,
+            paddingTop:0,
+          }
+  });
+  
   return (
-    <View>
-      <Text style={s`p-5 w-full h-full `}>App</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions}>
+      {tabConfig.map(route=>(
+      <Tab.Screen key={route.name} name={route.name} component={route.component} />
+    ))}
+    </Tab.Navigator>
+  </NavigationContainer>
   )
 }
 
