d2c3f677622e6c784a4c00761766e23102c9441c
sthaarekh <arekhshrestha@gmail.com>
2025-04-26 22:06:38 +0545

    Insert Data function added

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import SQLite from 'react-native-sqlite-storage'
+import { Alert } from 'react-native';
 
 const db = SQLite.openDatabase({
     name: 'LekhaDB',
@@ -8,7 +9,7 @@ const db = SQLite.openDatabase({
 error =>{console.log(error)}
 );
 
-const createTable=()=>{
+export const createTable=()=>{
     db.transaction((tx)=>{
         tx.executeSql(
             'CREATE TABLE IF NOT EXISTS expenseRec (id INTEGER PRIMARY KEY AUTOINCREMENT, category TEXT, description TEXT, amount REAL, payment_mode TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)',
@@ -18,6 +19,28 @@ const createTable=()=>{
             },
             error=>{
                 console.error("Error creating table")
+            }
+        )
+    })
+}
+
+export const insertData=(category, description, amount, mode, callback)=>{
+    db.transaction((tx)=>{
+        tx.executeSql(
+            'INSERT INTO userrec (category, description, amount, payment_mode) VALUES (?, ?, ?, ?)',
+            [category, description, parseFloat(amount), mode],
+            (tx, results) => {
+                if (results.rowsAffected > 0) {
+                    callback(true);
+                } else {
+                    Alert.alert('Error', 'Failed to add item');
+                    callback(false);
+                }
+                },
+            error => {
+                console.error('Error adding item:', error);
+                Alert.alert('Error', 'An error occurred while adding the item');
+                callback(false);
             }
         )
     })
